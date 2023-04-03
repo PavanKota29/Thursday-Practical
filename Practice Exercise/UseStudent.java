42ca774c9d961cd3f7d5dae7cd74a889f4160bf6
Pavan kota <91624021+PavanKota29@users.noreply.github.com>
2023-04-03 12:56:08 +0530

    Update UseStudent.java

--- Practice Exercise/UseStudent.java
+++ Practice Exercise/UseStudent.java
@@ -5,29 +5,29 @@ called "speak". Create a child class called "Student" that inherits from Person
 and has an additional attribute called "grade" and a method called "study". 
 Create an object of the Student class and call both the "speak" and "study" methods.*/
 
-class Person{
-	String name;
+class Person{//Parent class
+	String name;//data members
 	int age;
-	void speak() {
+	void speak() {//method
 		System.out.println("This is Person class method speak !!!");
 	}
 }
 
-class Student extends Person{
-	String grade;
-	void study() {
+class Student extends Person{//child class
+	String grade;//data members
+	void study() {//method
 		System.out.println("This is Student method study !!!");
 	}
 }
 
 
-class UseStudent {
+class UseStudent {//main class
 	public static void main(String[] args) {
-		Student s=new Student();
-		s.name="Pavan";
+		Student s=new Student();//object creating 
+		s.name="Pavan";//values assigning
 		s.age=21;
 		s.grade="A";
-		s.speak();
+		s.speak();//calling method
 		s.study();
 	}
 
